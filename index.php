79ce8f88fd99aaad6004549173fad648da99898a
codeproline <webdev410219@gmail.com>
2021-01-25 17:34:12 +0800

    change index

--- index.php
+++ index.php
@@ -6,6 +6,14 @@
  * @package WordPress
  */
 
+ /**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
+
+
 /**
  * Tells WordPress to load the WordPress theme and output it.
  *
